a8045248aa8995b57dcf3c353af926f287c6e9c0
SMEAG <bernieiwnl@gmail.com>
2023-05-29 13:57:32 +0700

    clean code react

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
 import { useState } from "react";
 import Expenses from "./components/Expense/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { Expense } from "./components/utils/ExpenseType";
+import { DUMMY_EXPENSES } from "./components/utils/DummyExpense";
 import "./App.css";
 
 function App() {
-  type Expense = {
-    id: string;
-    title: string;
-    amount: number;
-    date: Date;
-  };
-
-  const DUMMY_EXPENSES: Expense[] = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
   const [currentExpenses, setCurrentExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense: Expense) => {
